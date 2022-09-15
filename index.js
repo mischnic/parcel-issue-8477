34c85f06dc88555b82fe9d15029dfd61fd1aea7a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2022-09-15 11:59:58 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// const i18nModule = await import(
+//     "@atlassian/jira-portfolio-plan-wizard/dist/esm/i18n/*.js"
+// );
+// console.log(i18nModule);
+
+const x = import("./data/*.js");
+console.log(x);
